0e15b13c644c9b4c82ce9ef6a90b3029aa08fe9e
jake <jake@vertaxe.com>
2016-07-14 12:09:08 +0200

    use babel for gulpfile & add websocket-rails client

---
+++ gulpfile.babel.js
@@ -0,0 +1,43 @@
+'use strict';
+
+import gulp from 'gulp';
+import gutil from 'gulp-util';
+import gulpif from 'gulp-if';
+import uglify from 'gulp-uglify';
+import concat from 'gulp-concat';
+import yargs from 'yargs';
+import livereload from 'gulp-livereload';
+import babel from "gulp-babel";
+import sourcemaps from "gulp-sourcemaps";
+
+const source = {
+  js: [
+    'src/websocket_rails.0.0.1.min.js',
+    'node_modules/js-cookie/src/js.cookie.js',
+    'src/plugin.js'
+  ]
+}
+
+gulp.task('js', function() {
+
+  let isProduction = (yargs.argv.production === undefined) ? false : true;
+
+  gulp.src(source.js)
+    .pipe(sourcemaps.init())
+    .pipe(babel({
+      ignore: [
+        'src/websocket_rails.0.0.1.min.js'
+      ]
+    }))
+    .pipe(concat('latest.js'))
+    .pipe(gulpif(isProduction,uglify({ mangle: true })))
+    .pipe(sourcemaps.write("."))
+    .pipe(gulp.dest('dist'))
+    .pipe(livereload());
+
+});
+
+gulp.task('default', function () {
+  livereload.listen();
+  gulp.watch(['src/*.js'], ['js']);
+});
--- src/plugin.js
+++ src/plugin.js
@@ -61,25 +61,6 @@ var Reactions = {
 
     }
   },
-
-  ajaxGet(url) {
-    return new Promise(function(resolve, reject) {
-      let req = new XMLHttpRequest();
-      req.open("GET", url);
-      req.onload = function() {
-        if (req.status === 200) {
-            resolve(req.response);
-        } else {
-            reject(new Error(req.statusText));
-        }
-      };
-
-      req.onerror = function() {
-        reject(new Error("Network error"));
-      };
-
-      req.send();
-    });
-  }
+  dispatcher: new WebSocketRails('localhost:3000/websocket')
 
 }
